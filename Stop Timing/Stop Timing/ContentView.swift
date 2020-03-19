4040cb17f4274deacb9676b11d1b3bc9fd14a846
Surachet Y <aottapkub@hotmail.com>
2020-03-19 21:38:48 +0700

    Stopwatch.

--- Stop Timing/Stop Timing/ContentView.swift
+++ Stop Timing/Stop Timing/ContentView.swift
@@ -8,70 +8,63 @@
 
 import SwiftUI
 
-enum GameState {
-    case start
-    case stop
-    case again
-}
-
 struct ContentView: View {
     
-    @State var timeRemaining:Double = 0.0
+    @State var timeCount:Double = 0.0
     @State var startState = false
     @State var stopState = false
-    @State var againState = false
     var startText = "Start !"
     var stopText = "Stop !"
-    var againText = "Again !"
     
-    let timer = Timer.publish(every: 0.05, on: .main, in: .common)
+    var timer = Timer.publish(every: 0.01, on: .main, in: .default)
     
     var body: some View {
         ZStack {
             Color.yellow.edgesIgnoringSafeArea(.all)
             ZStack {
                 VStack {
-                    Text("\(timeRemaining, specifier: "%.1f")").font(.system(size: 120, weight: .bold, design: .default)).italic()
-                        .foregroundColor(.white).shadow(color: .secondary, radius: 5, x: 5, y: 5)
-                        .onReceive(timer) { _ in
-                            if self.timeRemaining >= 0 {
-                                self.timeRemaining += 0.1
+                    HStack {
+                        Group {
+                            Text("\(timeCount, specifier: "%.1f")").font(.system(size: 80, weight: .bold, design: .default)).italic()
+                                .foregroundColor(.white)
+                                .onReceive(timer) { _ in
+                                    if self.timeCount >= 0 {
+                                        self.timeCount += 0.1
+                                    }
                             }
+                            Text("s")
+                            .font(.system(size: 60, weight: .black))
+                            .italic()
+                            .foregroundColor(Color.white)
+                            .padding(.top)
+                        }.shadow(color: .secondary, radius: 5, x: 5, y: 5)
                     }
                     Spacer()
                     VStack {
                         if startState == false {
                             Button(startText) {
                                 self.startState.toggle()
-                                self.timer.connect()
-                                print(self.startState)
+                                if self.startState == true {
+                                    self.timer.connect()
+                                }
+//                                print(self.startState)
                             }.padding().padding(.leading, 40).padding(.trailing, 40)
                                 .background(Color.blue)
                                 .foregroundColor(.white).font(.system(size: 30, weight: .bold, design: .default)).cornerRadius(60)
                                 .padding()
                         }
-                        else if startState == true && stopState == false {
+                        else {
                             Button(stopText) {
                                 self.stopState.toggle()
                                 self.timer.connect().cancel()
                             }.padding().padding(.leading, 40).padding(.trailing, 40)
-                            .background(Color.blue)
+                            .background(Color.red)
                             .foregroundColor(.white).font(.system(size: 30, weight: .bold, design: .default)).cornerRadius(60)
                             .padding()
                         }
-                        else {
-                            Button(againText) {
-                                self.againState.toggle()
-                                self.timeRemaining = 0.0
-                                self.startState = false
-                                self.stopState = false
-                                print("\(self.startState) + \(self.stopState)")
-                            }.padding().padding(.leading, 40).padding(.trailing, 40)
-                            .background(Color.blue)
-                            .foregroundColor(.white).font(.system(size: 30, weight: .bold, design: .default)).cornerRadius(60)
-                            .padding()
+                        VStack {
+                            Text("Skyzwing ☁️ :  a little work with SwiftUI ").foregroundColor(Color.black.opacity(0.5)).font(.system(size: 14, weight: .bold, design: .default)).italic().padding()
                         }
-                        Text("Skyzwing").font(.caption).padding(.bottom, 50)
                     }
                 }
             }.padding(.top, 120)
