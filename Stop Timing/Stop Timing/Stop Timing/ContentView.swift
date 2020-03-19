1165f0b985e2c68de6066aa121b13c42b6d655cd
Surachet Y <aottapkub@hotmail.com>
2020-03-19 15:54:54 +0700

    80% update

---
+++ Stop Timing/Stop Timing/Stop Timing/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Stop Timing
+//
+//  Created by Surachat Yaitammasan on 19/3/2563 BE.
+//  Copyright © 2563 Surachat Yaitammasan. All rights reserved.
+//
+
+import SwiftUI
+
+enum GameState {
+    case start
+    case stop
+    case again
+}
+
+struct ContentView: View {
+    
+    @State var timeRemaining:Double = 0.00
+    @State var startState = false
+    var startText = "Start !"
+    var stopText = "Stop !"
+    var againText = "Again !"
+    
+    let timer = Timer.publish(every: 0.05, on: .main, in: .common)
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.edgesIgnoringSafeArea(.all)
+            ZStack {
+                VStack {
+                    Text("\(timeRemaining, specifier: "%.1f")").font(.system(size: 120, weight: .bold, design: .default)).italic()
+                        .foregroundColor(.white).shadow(color: .secondary, radius: 5, x: 5, y: 5)
+                        .onReceive(timer) { _ in
+                            if self.timeRemaining >= 0 {
+                                self.timeRemaining += 0.1
+                            }
+                    }
+                    Spacer()
+                    VStack {
+                        if startState == false {
+                            Button(startText) {
+                                self.startState.toggle()
+                                self.timer.connect()
+                                print(self.startState)
+                            }.padding().padding(.leading, 40).padding(.trailing, 40)
+                                .background(Color.blue)
+                                .foregroundColor(.white).font(.system(size: 30, weight: .bold, design: .default)).cornerRadius(60)
+                                .padding()
+                        }
+                        
+                        Text("Skyzwing").font(.caption).padding(.bottom, 50)
+                    }
+                }
+            }.padding(.top, 120)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
